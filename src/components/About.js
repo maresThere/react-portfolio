0713704506d77a2f720f709a9be16918cb14e241
Marianne Kelly <marianne_kelly@hotmail.com>
2017-04-17 10:17:43 -0400

    cleaned a few things up

--- src/components/About.js
+++ src/components/About.js
@@ -5,12 +5,11 @@ import light from '../images/light.png'
 
 class About extends Component {
   render () {
-   return <section className='aboutMe'>
-     <p><img src={glasses} height='40px' width='80px' />My background is in Property Management where I became skilled in sales, marketing and business operations. The characteristics I will bring to my employer are a positive attitude and a willingness to help as well as learn from others.</p><p><img src={theIronYardLogo} height='70px' width='70px' />
+    return <section className='aboutMe'>
+      <p><img src={glasses} height='40px' width='80px' />My background is in Property Management where I became skilled in sales, marketing and business operations. The characteristics I will bring to my employer are a positive attitude and a willingness to help as well as learn from others.</p><p><img src={theIronYardLogo} height='70px' width='70px' />
       I am currently a Front End Development student at The Iron Yard in St. Petersburg, Florida. My completion date is June 2, 2017.</p>
-     <p><img src={light} height='70px' width='78px' />I enjoy design and development. Creating an application that is both delightfully functional and aesthetically pleasing is very exciting to me.</p>
-   </section>
-
+      <p><img src={light} height='70px' width='78px' />I enjoy design and development. Creating an application that is both delightfully functional and aesthetically pleasing is very exciting to me.</p>
+    </section>
   }
 }
 
