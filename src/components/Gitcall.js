4541b7ea820aa6959009d2401a8d14b4b1805905
Marianne Kelly <marianne_kelly@hotmail.com>
2017-05-28 16:51:57 -0400

    links

--- src/components/Gitcall.js
+++ src/components/Gitcall.js
@@ -4,9 +4,9 @@ class Gitcall extends Component {
   render () {
     const links = this.props.work.map((repo, i) => {
       return <div className='project'>
-        {/* <a className={`pic${i + 1}`} href={repo.html_url} key={i}>
-          {repo.description}
-        </a> */}
+        <a className={`pic${i + 1}`} href={repo.html_url} key={i}>
+          <img src={GitHub} className='github' />
+        </a>
         <img src={`https://raw.githubusercontent.com/maresThere/${repo.name}/master/Kapture.gif`} className='Kapture' />
       </div>
     })
@@ -16,7 +16,8 @@ class Gitcall extends Component {
         <p>The Iron Yard taught me how to program with React components. Each assignment was an opportunity to improve css skills and organize code more efficiently.</p>
       </div>
       <ul className='myWork'>
-        <li className='work'>{links[0]}</li>
+        <li className='work'>{links[0]}
+        </li>
         <li className='work'>{links[1]}</li>
         <li className='work'>{links[2]}</li>
       </ul>
