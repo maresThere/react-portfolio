f75ee112709fcc8981f04f967c9392a93a7e23bc
Marianne Kelly <marianne_kelly@hotmail.com>
2017-06-06 05:15:06 -0400

    cleaning up

--- src/components/WebWork.js
+++ src/components/WebWork.js
@@ -6,18 +6,21 @@ import mtdora from '../images/mtdora.jpg'
 class WebWork extends Component {
   render () {
     return <div className='WebWork'>
-      <h3>DESIGN & DEVELOPMENT</h3>
+
+      <div className='pHeader'>
+        <h2>DESIGN & DEVELOPMENT</h2>
+      </div>
       <ul className='webContainer'>
         <li className='webEx'>
-          <p className='title'>Tarpon Springs Chamber </p>
+          <h3 className='title'>Tarpon Springs Chamber </h3>
           <img src={tarpon} alt='Tarpon Springs Chamber site' />
         </li>
         <li className='webEx'>
-          <p className='title'>Thomas Promise</p>
+          <h3 className='title'>Thomas Promise</h3>
           <img src={thomaspromise} alt='Thomas Promise' />
         </li>
         <li className='webEx'>
-          <p className='title'>Mount Dora Chamber Site</p>
+          <h3 className='title'>Mount Dora Chamber Site</h3>
           <img src={mtdora} alt='TMount Dora Chamber site' />
         </li>
       </ul>
