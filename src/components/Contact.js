372451c5655b94b78f5b6b98344ee6b690bf03ae
Marianne Kelly <marianne_kelly@hotmail.com>
2017-06-27 11:07:25 -0400

    fixed link

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -18,7 +18,7 @@ class Contact extends Component {
         </div>
         <ul className='smLinks'>
           <li><a href='https://www.linkedin.com/in/marekelly'target='_blank'><i className='fa fa-linkedin-square fa-2x' aria-hidden='true' /></a></li>
-          <li><a href='https://github.com/maresThere'target='_blank'><i className='fa fa-codepen fa-2x' aria-hidden='true' /></a></li>
+          <li><a href='https://codepen.io/maresThere/'target='_blank'><i className='fa fa-codepen fa-2x' aria-hidden='true' /></a></li>
           <li><a href='https://github.com/maresThere'target='_blank'><i className='fa fa-github-square fa-2x' aria-hidden='true' /></a></li>
           <li><a href='https://www.instagram.com/marekellymk/'target='_blank'><i className='fa fa-instagram fa-2x' aria-hidden='true' /></a></li>
           <li><a href='https://twitter.com/mareKellyWebDev' target='_blank'><i className='fa fa-twitter-square fa-2x' aria-hidden='true' /></a></li>
--- src/components/Decor.js
+++ src/components/Decor.js
@@ -4,7 +4,7 @@ class Decor extends Component {
   render () {
     return <ul className='smLinksB'>
       <li><a href='https://www.linkedin.com/in/marekelly'target='_blank'><i className='fa fa-linkedin-square fa-lg' aria-hidden='true' /></a></li>
-      <li><a href='https://github.com/maresThere'target='_blank'><i className='fa fa-codepen fa-lg' aria-hidden='true' /></a></li>
+      <li><a href='https://codepen.io/maresThere/'target='_blank'><i className='fa fa-codepen fa-lg' aria-hidden='true' /></a></li>
       <li><a href='https://github.com/maresThere'target='_blank'><i className='fa fa-github-square fa-lg' aria-hidden='true' /></a></li>
       <li><a href='https://www.instagram.com/marekellymk/'target='_blank'><i className='fa fa-instagram fa-lg' aria-hidden='true' /></a></li>
       <li><a href='https://twitter.com/mareKellyWebDev' target='_blank'><i className='fa fa-twitter-square fa-lg' aria-hidden='true' /></a></li>
