2ca7d4683f21cb92a97d46b26b5967f4482e2457
Marianne Kelly <marianne_kelly@hotmail.com>
2017-06-28 11:45:38 -0400

    animation stars

--- src/components/App.js
+++ src/components/App.js
@@ -67,7 +67,7 @@ class App extends Component {
         </div>
         <Nav />
         <Footer />
-        <Decor />
+        {/* <Decor /> */}
       </div>
     </Router>
   }
--- src/components/Intro.js
+++ src/components/Intro.js
@@ -4,14 +4,28 @@ import mareKelly from '../images/mareKelly.jpeg'
 class Intro extends Component {
   render () {
     return (
-      <div className='introContainer bggg'>
+      <div className='introContainer'>
+        <h2 className='hello'>HELLO</h2>
         <div className='introBox'>
-          <h2 className='hello'>HELLO</h2>
           <img src={mareKelly} />
           <div className='introText'>
             <h1>{this.props.name}</h1>
             <p>{this.props.bio}</p>
           </div>
+        </div>
+        <div className='starzContainer'>
+          <figure className='star'>
+            <figure className='star-top' />
+            <figure className='star-bottom' />
+          </figure>
+          <figure className='star2'>
+            <figure className='star-top' />
+            <figure className='star-bottom' />
+          </figure>
+          <figure className='star3'>
+            <figure className='star-top' />
+            <figure className='star-bottom' />
+          </figure>
         </div>
       </div>
     )
