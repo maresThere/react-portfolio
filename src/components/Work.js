44add472aecaccd2ba05d83c0640edb2b24534c1
Marianne Kelly <marianne_kelly@hotmail.com>
2018-06-24 14:24:30 -0400

    adding current work

--- src/components/Work.js
+++ src/components/Work.js
@@ -1,12 +1,19 @@
 import React, { Component } from 'react'
 import WebWork from './WebWork'
 import portfolio from '../workSamples'
+import currentWork from '../currentSamples'
 
 class Work extends Component {
   render () {
     return (
       <div className='dark-gray-bg'>
         <ul className='light-gray-bg'>
+          <h2>Latest Professional Work</h2>
+          <h4>Production work in the Marketing department mainly consists of creating single landing pages. styling and javascript are kept on the index page and the bootstrap cdn is linked for responsiveness.</h4>
+          {currentWork.map(item => <WebWork key={item.name} details={item} />)}
+        </ul>
+        <ul className='light-gray-bg'>
+          <h2>Personal Development &amp; Past Work</h2>
           {portfolio.map(item => <WebWork key={item.name} details={item} />)}
         </ul>
       </div>
---
+++ src/currentSamples.js
@@ -0,0 +1,31 @@
+const currentWork = [
+
+  {
+    name: 'Webinar Sales Pages',
+    image: '/images/webinar.jpg',
+    desc:
+      '',
+    git: 'https://github.com/maresThere/cyptoWebinar',
+    link: 'http://lavish-fan.surge.sh/'
+  },
+
+  {
+    name: 'Landing Pages',
+    image: '/images/exp-offer.jpg',
+    desc:
+      'Developed a generic "expired offer" landing page to use in Salesforce CRM. ',
+    git: '',
+    link: 'http://picayune-arm.surge.sh'
+  },
+
+  {
+    name: 'Opt-in pages',
+    image: '/images/ideas-optin.jpg',
+    desc: '',
+    git: '',
+    link: 'http://pages.exct.tradesmith.com/page.aspx?QS=5c591a8916642e73c78705da9cf64a4f384b223ca763a459e2c1464239253855'
+
+  }
+]
+
+export default currentWork
